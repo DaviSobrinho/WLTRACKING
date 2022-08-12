e37b2c3744a8563bdc1aa55d4f360e13865a4196
Davi Sobrinho <davisobrinho82452@gmail.com>
2022-08-11 23:03:25 -0300

    Main build for WLTRACKING project, actually with insert and delete all function for room db, I finally discovered how to add a TabListener, so now we can navigate through the views and the recycler view will be automatically loaded when we switch to the right tab(now, only configured to the "clientesrecyclerview)". Now I'll create a new Activity for the insertion of other entities from the db.

---
+++ app/src/main/java/com/example/wltrackingmobile/AddActivity.kt
@@ -0,0 +1,15 @@
+package com.example.wltrackingmobile
+
+import android.os.Bundle
+import android.transition.Explode
+import android.transition.Slide
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+
+class AddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+
+    }
+}
